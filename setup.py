897f0528d38d943c68f5deaeb60449dbe4aed64c
Leonardo P Tizzei <leonardo.tizzei@gmail.com>
2024-05-09 21:55:39 -0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+# configuration approach followed:
+# - whenever possible, prefer pyproject.toml
+# - for configurations insufficiently supported by pyproject.toml, use setup.cfg instead
+# - setup.py discouraged; minimal stub included only for compatibility with legacy tools
+
+# Minimal stub for backwards compatibility, e.g. for legacy tools without PEP 660 support.
+# See https://setuptools.pypa.io/en/latest/userguide/quickstart.html
+from setuptools import setup
+
+setup()
