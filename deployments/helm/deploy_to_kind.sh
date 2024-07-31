2f7ad1181e083b63b3b50fa973868e19c686a5f0
Romeo Kienzler <romeo.kienzler1@ibm.com>
2024-07-31 11:26:22 +0200

    move port-forwards to respawning processes

--- deployments/helm/deploy_to_kind.sh
+++ deployments/helm/deploy_to_kind.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 helm install -f values-kind.yaml kind .
-sleep 10
-kubectl port-forward service/dask-scheduler 8787:8787 &
-kubectl port-forward service/openeo-geodn-driver 9091:9091 &
-kubectl port-forward service/pgstac-fastapi 8080:8080 &
-kubectl port-forward service/pgstacpostgisservice 5432:5432 &
-kubectl port-forward service/stac-explorer 8081:8080 &
+(while true; do  kubectl port-forward service/dask-scheduler 8787:8787; done) &
+(while true; do  kubectl port-forward service/openeo-geodn-driver 9091:9091; done) &
+(while true; do  kubectl port-forward service/pgstac-fastapi 8080:8080; done) &
+(while true; do  kubectl port-forward service/pgstacpostgisservice 5432:5432; done) &
+(while true; do  kubectl port-forward service/stac-explorer 8081:8080; done) &
+
 
 
