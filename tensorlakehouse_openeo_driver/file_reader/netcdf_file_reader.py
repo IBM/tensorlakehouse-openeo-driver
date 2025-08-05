c3f10e7f8cd21d6bc8ffc45fae084ee62ebd4307
Leonardo P Tizzei <leonardo.tizzei@gmail.com>
2025-08-05 09:31:08 -0300

    change the name of time dimension

--- tensorlakehouse_openeo_driver/file_reader/netcdf_file_reader.py
+++ tensorlakehouse_openeo_driver/file_reader/netcdf_file_reader.py
@@ -120,7 +120,7 @@ class NetCDFFileReader(RasterFileReader):
             data_arrays.append(da)
         if len(data_arrays) > 1:
             # concatenate all xarray.DataArray objects
-            data_array = xr.concat(data_arrays, dim=DEFAULT_TIME_DIMENSION)
+            data_array = xr.concat(data_arrays, dim=time_dim)
         else:
             data_array = data_arrays.pop()
         # filter by area of interest
